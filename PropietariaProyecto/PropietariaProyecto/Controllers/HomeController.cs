bb142376904cdd19bd7d81cd2389387a1941fef2
Manuel Marinez <marinezbay94@gmail.com>
2017-07-29 21:14:41 -0400

    Hosting, Azure hosting

--- PropietariaProyecto/PropietariaProyecto/Controllers/HomeController.cs
+++ PropietariaProyecto/PropietariaProyecto/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -26,5 +28,10 @@ namespace PropietariaProyecto.Controllers
 
             return View();
         }
+
+        IFirebaseConfig config = new FirebaseConfig
+        {
+
+        };
     }
 }
